5b11ea0c6e48e8f6aa84bd2b4a8e6dfd757ff10a
siddhansh-creator <80710764+siddhansh-creator@users.noreply.github.com>
2022-04-05 00:46:55 +0530

    pagination

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,4 @@
+import { Divider, IconButton, InputBase, Paper } from '@mui/material'
 import React from 'react'
 // import './src/Components/Header.css';
 <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
@@ -5,13 +6,38 @@ import React from 'react'
 export default function Header() {
 	
     return (
-		
+		<>
         <div>
           	<nav class="navigation">
 			  <h1> Contact List</h1>
-		</nav>
+              </nav> 
+
+
+    <Paper
+      component="form"
+      sx={{ m: '10px 20px', p: '6px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+      <IconButton sx={{ p: '10px' }} aria-label="menu">
+        
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Contacts"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+    
+      </IconButton>
+     
+    </Paper>
+    
+    	<br/>
 		
         </div>
-		
+        </>	
     )
+
+  
 }
+
+	
